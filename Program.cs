b1ce909f167ac65a629f68bed5d24980ab589e3b
Aligndzcyln <34925324+Aligndzcyln@users.noreply.github.com>
2019-04-08 16:19:28 +0300

    tamsayı ve program cs
    tamsayı ve program cs classları yapıldı ve eklendi

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Proje1_2019
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double toplamGirdi=0;
+            double hataliOrani=0 ;
+            double gecerliOrani=0;
+            
+
+            //OBJELER OLUSTURULUP AYRI AYRI DEGERLER 1 DOSYADAN ÇEKİLİYOR
+            NoktaliSayi z = new NoktaliSayi();
+            z = z.okuma();
+            for(int i=0;i<z.ElemanSayisi;i++)
+            {
+                Console.WriteLine(z.Dizi100[i].Deger+" "+z.HataliSayisi);
+            }
+            TamSayi t = new TamSayi();
+            t = t.okuma1();
+            for(int i=0;i<t.ElemanSayisi;i++)
+            {
+                Console.WriteLine(t.Array1[i].Deger1+" "+t.HataliSayisi);
+                
+            }
+
+
+            //BU OBJELER AYRI SORT CLASSLARINA GONDERİLİP SİRALANIYOR (console writeline lar debug içindir!!!!!)
+            DoubleSort d = new DoubleSort(z);
+            z = d.Sirala();
+            for(int i=0;i<z.ElemanSayisi;i++)
+            {
+                Console.WriteLine(z.Dizi100[i].Deger);
+            }
+            z.yazdir();
+
+            IntSort test = new IntSort(t);
+            t = test.Sirala1();
+            for(int i=0;i<t.ElemanSayisi;i++)
+            {
+                Console.WriteLine(t.Array1[i].Deger1);
+            }
+            t.yazdir();
+
+
+            //Son çıktı için işlemler
+            toplamGirdi = t.HataliSayisi + t.ElemanSayisi + z.ElemanSayisi;//sadece t nin hatali sayisini aldik cunku iki objede hatali girdiyi eşit şekilde tutuyor
+
+            hataliOrani = t.HataliSayisi / toplamGirdi;
+            gecerliOrani = (t.ElemanSayisi + z.ElemanSayisi) / toplamGirdi;
+
+            Console.WriteLine("\n"+"ToplamSatir= "+toplamGirdi+"\n"+"Gecerli sayisi= "+(t.ElemanSayisi+z.ElemanSayisi)+"\n"
+                +"HataliSayisi= "+z.HataliSayisi
+                +Environment.NewLine+"Hata Orani= " + hataliOrani+Environment.NewLine+
+                "Gecerli Orani= "+gecerliOrani);
+           
+
+
+
+
+                        
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ TamSayi.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Proje1_2019
+{
+    class TamSayi:Sayi
+    {
+        TamSayi[] Array= new TamSayi[100];
+        int deger1;
+        int elemanSayisi;
+        int hataliSayisi;
+        public TamSayi()
+        {
+
+        }
+        public int Deger1
+        {
+            get
+            {
+                return deger1;
+            }
+            set
+            {
+                deger1 = value;
+            }
+        }
+        public int ElemanSayisi
+        {
+            get
+            {
+                return elemanSayisi;
+            }
+            set
+            {
+                elemanSayisi = value;
+            }
+        }
+        public int HataliSayisi
+        {
+            get
+            {
+                return hataliSayisi;
+            }
+            set
+            {
+                hataliSayisi = value;
+            }
+        }
+        
+        public TamSayi[] Array1{ get => Array; set => Array = value; }
+
+
+
+
+        public override NoktaliSayi okuma()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public override void yazdir()
+        {
+            StreamWriter yaz = new StreamWriter("SiralanmisTamSayilar.txt");
+
+            for (int i = 0; i < this.ElemanSayisi; i++)
+            {
+                yaz.WriteLine(this.Array1[i].Deger1.ToString());
+            }
+            yaz.Close();
+        }
+
+        public override TamSayi okuma1()
+        {
+            char[] hatalilar = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'Y', 'Z'
+            , 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'ı', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'y', 'z'
+            , '"', ' ', '^', '+', '%', '&', '/', '(', ')', '=', '?', '*', '-', '|', '_', '}', ']', '[', '{', '$', '#', 'é', '@', '<', '>', '.', ':', ';', '`', '´', '!'};//Hatalı sayılacak girdiler
+
+            TamSayi a = new TamSayi();
+            string line;
+            FileStream akis;
+            StreamReader Okuma;
+            string Yol = "sunnyboy.txt";
+            akis = new FileStream(Yol, FileMode.Open, FileAccess.Read);
+            Okuma = new StreamReader(akis);
+            line = Okuma.ReadLine();
+            string deneme;
+            bool control = true;
+            bool noktalidir = false;
+            int eleman = 0;
+
+            while (line != null)
+            {
+                control = true;
+                noktalidir = false;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == ',')
+                    {
+                        noktalidir = true;
+                    }
+                    for (int j = 0; j < hatalilar.Length; j++)
+                    {
+                        if (line[i].Equals(hatalilar[j]))
+                        {
+                            a.HataliSayisi++;
+                            control = false;
+                            break;
+                        }
+                    }
+                    if(control==false)
+                    {
+                        break;
+                    }
+                }
+                if (control != false && noktalidir == false)
+                {
+                    a.Array1[eleman] = new TamSayi();
+                    a.Array1[eleman].Deger1 = Convert.ToInt32(line);
+
+
+                    eleman++;
+                    a.elemanSayisi++;
+                }
+
+
+
+                line = Okuma.ReadLine();
+
+            }
+            akis.Close();
+            return a;
+        }
+    }
+}
