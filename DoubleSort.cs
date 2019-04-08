db087dfa37dce5f428967bed9c7c7b247f69a700
BurakGomec <48028551+BurakGomec@users.noreply.github.com>
2019-04-08 16:30:52 +0300

    Double sort ve int sort csleri

---
+++ DoubleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje1_2019
+{
+    class DoubleSort:Sorting
+    {
+        NoktaliSayi asArray = new NoktaliSayi();
+        public DoubleSort(NoktaliSayi gelecek)
+        {
+            asArray = gelecek;
+        }
+
+        public override NoktaliSayi Sirala()
+        {
+            for (int i = 0; i < asArray.ElemanSayisi - 1; i++)
+            {
+                for (int j = i + 1; j < asArray.ElemanSayisi; j++)
+                {
+                    if (asArray.Dizi100[j].Deger < asArray.Dizi100[i].Deger)
+                    {
+                        double degistir = asArray.Dizi100[(j)].Deger;
+                        asArray.Dizi100[j].Deger = asArray.Dizi100[i].Deger;
+                        asArray.Dizi100[i].Deger = degistir;
+                    }
+
+                }
+            }
+            
+            return asArray;
+
+
+        }
+        public override TamSayi Sirala1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IntSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje1_2019
+{
+    class IntSort:Sorting
+    {
+        TamSayi asArray = new TamSayi();
+        public IntSort(TamSayi array)
+        {
+            asArray = array;
+        }
+
+        public override NoktaliSayi Sirala()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override TamSayi Sirala1()
+        {
+            for (int i = 0; i < asArray.ElemanSayisi - 1; i++)
+            {
+                for (int j = i + 1; j < asArray.ElemanSayisi; j++)
+                {
+                    if (asArray.Array1[j].Deger1 < asArray.Array1[i].Deger1)
+                    {
+                        int degistir = asArray.Array1[j].Deger1;
+                        asArray.Array1[j].Deger1 = asArray.Array1[i].Deger1;
+                        asArray.Array1[i].Deger1 = degistir;
+                    }
+
+                }
+            }
+
+            return asArray;
+        }
+
+
+    }
+}
